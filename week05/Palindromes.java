a68b51c8bb4483d9157c6d9c75a075c29f6644c3
Daniel Bauer <bauer@cs.columbia.edu>
2023-02-15 10:55:36 -0500

    detecting palindromes using a stack

--- week05/Palindromes.java
+++ week05/Palindromes.java
@@ -3,8 +3,21 @@ import java.util.LinkedList;
 public class Palindromes {
 
   public static boolean isPalindrome(String s) {
-  
+ 
+    LinkedList<Character> stack = new LinkedList<>();
+    
+    int i =0;
+    for (; i < s.length() / 2; i++) 
+      stack.push(s.charAt(i)); 
 
+    if (s.length() % 2 == 1)
+      i++; 
+
+    for (; i < s.length(); i++)
+      if (stack.pop() != s.charAt(i))
+        return false; 
+
+    return stack.isEmpty();
   }
 
 
