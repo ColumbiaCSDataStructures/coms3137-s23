0974812dba361630eba21deefdcce52bc7f3f18c
Daniel Bauer <bauer@cs.columbia.edu>
2023-03-07 09:22:51 -0500

    in class changes

--- week06/BinaryTree.scala
+++ week06/BinaryTree.scala
@@ -13,6 +13,17 @@ abstract class BinaryTree[A]{
      * proc_node and proc_leaf and passing them to traverse.
      * The abstract method is implemented in Node and Leaf. 
      */                     
+    def traverse[B](proc_node : (B, B, A) => B, proc_leaf : A=>B) : B = {
+      
+      this match {
+        case Leaf(content) => proc_leaf(content); 
+        case Node(content, left, right) => proc_node(left.traverse(proc_node, proc_leaf),
+                                                right.traverse(proc_node, proc_leaf),
+                                                content);
+      }
+
+    } 
+
 }
 
 /** 
@@ -25,6 +36,7 @@ case class Node[A](val content: A, val left: BinaryTree[A], val right: BinaryTre
       * results returned by calling traverse recusively on each 
       * subtree and the content of this node. 
       */
+
 }
 
 /** 
@@ -40,10 +52,39 @@ case class Leaf[A](val content : A) extends BinaryTree[A] {
 
 object BinaryTree {
 
+
+  def postorder(t : BinaryTree[String]) : String = {
+    
+    t match {
+      case Leaf(c) => c;  
+      case Node(c, l, r) => postorder(l) + postorder(r) + c; 
+    }
+
+  }
+
+  /*def sumList(li : List[Integer]) : Integer = {
+
+    if (li == Nil) 
+      0
+    else 
+      sumList(li.tail) + li.head;
+
+    li match {
+      case Nil => 0;
+      case ::(head, tail)  => sum_list(tail) + head; 
+    }
+
+  }*/
+
+
+
   def main(args : Array[String]) = {
 
-     val t : BinaryTree[String]  = Node("C", Leaf("A"), Leaf("B"))
+     val t : BinaryTree[Int]  = Node(2, Leaf(3), Node(1, Leaf(2), Leaf(2)));
 
+     val tsum = t.traverse((parent : Int , lsum : Int, rsum : Int) => parent + lsum + rsum, (a : Int) => a); 
+
+     println(tsum); 
   }
 
 }
